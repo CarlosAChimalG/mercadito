15a9e0418b4d85b793fe9e027f7b57e8f0de7841
CarlosAChimalG <chimalcarlos261198@gmail.com>
2023-11-28 15:16:19 -0600

    test Rol

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .models import *
+
+class RolTest(TestCase):
+    def setUp(self):
+        self.rol = Rol.objects.create(idRol=1, nameRol="Admin")
+
+    def test_rol_name(self):
+        self.assertEqual(self.rol.nameRol, "Admin")
